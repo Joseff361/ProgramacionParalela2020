6861dee60a9dbc30c43a8a6e8630132f72085543
JUANFT11 <jfernandeztuni@gmail.com>
2020-02-05 19:45:18 -0500

    Actualizacion

--- Solucionariopractica01/PrimeraPracticaCalificadaPP/src/Problema7.cpp
+++ Solucionariopractica01/PrimeraPracticaCalificadaPP/src/Problema7.cpp
@@ -18,7 +18,7 @@ MPI_Init(&argc, &argv);
 MPI_Comm_size(MPI_COMM_WORLD,&size);
 MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 int tam,c,ntam,my_f;
-int my_factorial=1, our_factorial;
+long long int my_factorial=1, our_factorial;
 if(rank==0){
 	cout<<"Digite el factorial a operar: ";
 	cin>>tam;
@@ -43,7 +43,7 @@ for(int i=0; i<ntam;i++){
 	my_factorial=my_factorial*(my_f-i);
 }
 
-MPI_Reduce(&my_factorial,&our_factorial,1,MPI_INT,MPI_PROD,0,MPI_COMM_WORLD);
+MPI_Reduce(&my_factorial,&our_factorial,1,MPI_LONG_LONG_INT,MPI_PROD,0,MPI_COMM_WORLD);
 
 if(rank==0){
 	cout<<"El valor del factorial es: "<<our_factorial<<endl;
